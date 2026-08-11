669365c1dd0a64204e468dae6de2cfb8a8c23a19
pallavi22092006 <pallavigowda0169@gmail.com>
2026-08-11 14:13:43 +0530

    Create 4369-EvenNumberOfKnightMoves

---
+++ 4369-EvenNumberOfKnightMoves/4369-EvenNumberOfKnightMoves.java
@@ -0,0 +1,8 @@
+// Last updated: 8/11/2026, 2:13:42 PM
+class Solution {
+    public boolean canReach(int[] start, int[] target) {
+       int startColor=(start[0]+start[1])%2;
+        int targetColor=(target[0]+target[1])%2;
+        return startColor==targetColor;
+    }
+}
